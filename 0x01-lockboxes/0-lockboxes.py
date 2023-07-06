77449ddf1e854c2772ccedfb252aea66705c7502
kel2ck <kelvinonumajuru@gmail.com>
2023-07-06 02:37:10 -0700

    add 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,13 +5,13 @@ Solves the lock boxes challenge
 
 
 def canUnlockAll(boxes):
-    unlocked = [0]
+    unlocked = set([0])
+
     for box_id, box in enumerate(boxes):
-        if not box:
+        if not box_id in unlocked:
             continue
         for key in box:
-            if key < len(boxes) and key not in unlocked and key != box_id:
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+            if key < len(boxes):
+                unlocked.add(key)
+
+    return len(unlocked) == len(boxes)
