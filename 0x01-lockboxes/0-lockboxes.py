09fedda035c2a06ae71df06599604a14fba0a251
kel2ck <kelvinonumajuru@gmail.com>
2023-07-06 02:24:55 -0700

    add 0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Solve the lock boxes challenge
+"""
+
+
+def canUnlockAll(boxes):
+    unlocked = set([0])
+
+    for box_id, box in enumerate(boxes):
+        if not box_id in unlocked:
+            continue
+        for key in box:
+            if key < len(boxes):
+                unlocked.add(key)
+
+    return len(unlocked) == len(boxes)
